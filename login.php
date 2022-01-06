6eba067646f8ad7dda10d0996d6c404bdb7c0512
Alen Šimec <alen@tvz.hr>
2022-01-06 22:35:38 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,17 @@
 <?php 
+
+error_reporting(0);
+@ini_set('display_errors', 0);
+
+# Stop Hacking attempt
+define('__APP__', TRUE);
+
+	# Start session
+    session_start();
+	
+	# Database connection
+	include ("db.php");
+
 	print '
 	<h1>Sign In form</h1>
 	<div id="signin">';
@@ -20,13 +33,13 @@
 	else if ($_POST['_action_'] == TRUE) {
 		
 		$query  = "SELECT * FROM users";
-		$query .= " WHERE username='" .  $_POST['username'] . "' AND archive='N'";
-		$result = @mysqli_query($MySQL, $query);
+		$query .= " WHERE username='" .  $_POST['username'] . "'";
+		$result = @mysqli_query($MySQL, $query); print $query;
 		$row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
 		
-		if (password_verify($_POST['password'], $row['password'])) {
+		if ($row['password'] = $_POST['password']) {
 			#password_verify https://secure.php.net/manual/en/function.password-verify.php
-			$_SESSION['user']['valid'] = 'true';
+			$_SESSION['user']['valid'] = 'true'; 
 			$_SESSION['user']['id'] = $row['id'];
 			# 1 - administrator; 2 - editor; 3 - user
 			$_SESSION['user']['role'] = $row['role'];
@@ -34,14 +47,14 @@
 			$_SESSION['user']['lastname'] = $row['lastname'];
 			$_SESSION['message'] = '<p>Dobrodošli, ' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname'] . '</p>';
 			# Redirect to admin website
-			header("Location: index.php?menu=7");
+			header("Location: photography.php?menu=7");
 		}
 		
 		# Bad username or password
 		else {
 			unset($_SESSION['user']);
 			$_SESSION['message'] = '<p>You entered wrong email or password!</p>';
-			header("Location: index.php?menu=6");
+			header("Location: photography.php?menu=6");
 		}
 	}
 	print '
